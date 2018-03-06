d2cd75e2e70b8408efe20f25b1f4a5d0ad7e2d31
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-06 14:25:40 -0700

    Imaging for SwooshApp

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Landon Carr on 3/6/18.
+//  Copyright © 2018 Landon Carr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
